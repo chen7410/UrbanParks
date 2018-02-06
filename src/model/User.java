5aafa8b323a220d7e08857cb7c287271061be248
Matthew Chen <matthewfory@gmail.com>
2018-02-06 01:31:00 -0800

    add user type to User; implement getUser() and addUser() in UserMap;

--- src/model/User.java
+++ src/model/User.java
@@ -1,39 +1,67 @@
 package model;
 
+/**
+ * This class represents a single user in the system.
+ * @author Group 7
+ *
+ */
 public abstract class User {
 	private String myUserName;
 
 	private String myFirstName;
 
 	private String myLastName;
+	
+	private String myUserType;
 
 	public User(final String theUserName, final String theFirstName, final String theLastName) {
 		myUserName = theUserName;
 		myFirstName = theFirstName;
 		myLastName = theLastName;
+		myUserType = "undefined";
 	}
 
 	public String getMyUserName() {
 		return myUserName;
 	}
 
-	public void setMyUserName(final String myUserName) {
-		this.myUserName = myUserName;
+	public void setMyUserName(final String theUserName) {
+		this.myUserName = theUserName;
 	}
 
 	public String getMyFirstName() {
 		return myFirstName;
 	}
 
-	public void setMyFirstName(final String myFirstName) {
-		this.myFirstName = myFirstName;
+	public void setMyFirstName(final String theFirstName) {
+		this.myFirstName = theFirstName;
 	}
 
 	public String getMyLastName() {
 		return myLastName;
 	}
 
-	public void setMyLastName(final String myLastName) {
-		this.myLastName = myLastName;
+	public void setMyLastName(final String theLastName) {
+		this.myLastName = theLastName;
+	}
+	
+	public final String getMyUserType() {
+		return myUserType;
+	}
+
+	public final void setMyUserType(final String theUserType) {
+		this.myUserType = theUserType;
+	}
+	
+	/**
+	 * Display an user's information.
+	 */
+	@Override
+	public String toString() {
+		return "User type: " + myUserType +
+				" | User name: " + myUserName + 
+				" | First name: " + myFirstName + 
+				" | Last name: " + myLastName;
+		
 	}
 }
