270274d4369e8689b2bf57a02aadee2b9eaf599a
Tuan Dinh <tuandinhsemail@gmail.com>
2018-02-07 23:40:13 -0800

    completed login

--- src/UI/UrbanParkUI.java
+++ src/UI/UrbanParkUI.java
@@ -8,67 +8,85 @@ package UI;
 
 import java.util.Scanner;
 
+import model.ParkManager;
+import model.User;
+import model.UserMap;
+
 public class UrbanParkUI {
-	
+
 	private static Scanner myScanner = new Scanner(System.in);
-	
+
+	private static User myCurrentUser;
+
+	private static UserMap myUsers;
+
 	public static void main(final String[] theArgs) {
+		load();
 		int task = welcome();
-		switch(task) {
-			case 1: logIn();
-					break;
-			
+		switch (task) {
+		case 1:
+			logIn();
+			break;
+		case 2:
+			System.out.println(">>> Thank you for using Urban Parks.");
+			myScanner.close();
+		default:
+			break;
 		}
-		
-		// Only if the user is a park manager.
-		basicParkManagerOptions();
+
+		if (myCurrentUser instanceof ParkManager) {
+			basicParkManagerOptions();
+		}
 	}
-	
+
+	private static void load() {
+		myUsers = new UserMap();
+	}
+
 	/**
-	 * Prints out the welcome statements, prompts the user for next
-	 * task, and returning task value.
+	 * Prints out the welcome statements, prompts the user for next task, and
+	 * returning task value.
 	 * 
 	 * @return The task value the user selected.
 	 */
 	private static int welcome() {
 		System.out.println(">>> Welcome to Urban Parks.");
 		System.out.println(">>> What would you like to do? (Please select a number)");
-		
-		String[] options = {"Log in", "Exit"};
+
+		String[] options = { "Log in", "Exit" };
 		for (int i = 0; i < options.length; i++) {
 			System.out.println("    " + (i + 1) + ". " + options[i]);
 		}
-		
+
 		System.out.println();
 		System.out.print("> ");
 		int task = myScanner.nextInt();
 		System.out.println();
-		
+
 		return task;
 	}
-	
-	// *** ISSUE: Once the user inputs their username, the their data (First & Last name)"
-	//																	+ " needs to be retrieved & added here.***"
+
+
 	private static void logIn() {
 		System.out.println(">>> Please enter your username:");
-		
+
 		System.out.println();
 		System.out.print("> ");
-		String username = myScanner.next();
+		String username = myScanner.nextLine();
 		System.out.println();
-		
-		// This print statement below needs to be fixed.
-		System.out.println(">>> Welcome " + username + ". You are logged in as a ...");
+		myCurrentUser = myUsers.getUser(username);
+		System.out.println(">>> Welcome " + myCurrentUser.getFirstName() + " " + myCurrentUser.getLastName()
+				+ ". You are logged in as a " + myCurrentUser.getUserType());
 	}
-	
+
 	private static void basicParkManagerOptions() {
 		System.out.println(">>> What would you like to do? (Please select a number)");
-		
-		String[] options = {"Submit a new job", "View all your submitted jobs", "Log out"};
+
+		String[] options = { "Submit a new job", "View all your submitted jobs", "Log out" };
 		for (int i = 0; i < options.length; i++) {
 			System.out.println("    " + (i + 1) + ". " + options[i]);
 		}
-		
+
 		System.out.println();
 		System.out.print("> ");
 		int task = myScanner.nextInt();
