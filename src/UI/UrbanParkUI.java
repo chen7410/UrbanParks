3df1e5ec3fb8e57ef9efa1ea2921dc6e952b3bf4
brookn3 <brooknegussie2014@gmail.com>
2018-02-07 18:54:43 -0800

    Added logIn() and basicParkManagerOptions() as well as formatting the main().

--- src/UI/UrbanParkUI.java
+++ src/UI/UrbanParkUI.java
@@ -1,3 +1,9 @@
+/**
+ * T CSS 360 - Winter 2018
+ * Team: Group 7
+ * Urban Parks Project
+ */
+
 package UI;
 
 import java.util.Scanner;
@@ -8,7 +14,14 @@ public class UrbanParkUI {
 	
 	public static void main(final String[] theArgs) {
 		int task = welcome();
+		switch(task) {
+			case 1: logIn();
+					break;
+			
+		}
 		
+		// Only if the user is a park manager.
+		basicParkManagerOptions();
 	}
 	
 	/**
@@ -32,5 +45,33 @@ public class UrbanParkUI {
 		System.out.println();
 		
 		return task;
+	}
+	
+	// *** ISSUE: Once the user inputs their username, the their data (First & Last name)"
+	//																	+ " needs to be retrieved & added here.***"
+	private static void logIn() {
+		System.out.println(">>> Please enter your username:");
+		
+		System.out.println();
+		System.out.print("> ");
+		String username = myScanner.next();
+		System.out.println();
+		
+		// This print statement below needs to be fixed.
+		System.out.println(">>> Welcome " + username + ". You are logged in as a ...");
+	}
+	
+	private static void basicParkManagerOptions() {
+		System.out.println(">>> What would you like to do? (Please select a number)");
+		
+		String[] options = {"Submit a new job", "View all your submitted jobs", "Log out"};
+		for (int i = 0; i < options.length; i++) {
+			System.out.println("    " + (i + 1) + ". " + options[i]);
+		}
+		
+		System.out.println();
+		System.out.print("> ");
+		int task = myScanner.nextInt();
+		System.out.println();
 	}
 }
