048e32761e48d4919bda34c0be5b3d2628080ee9
Tuan Dinh <tuandinhsemail@gmail.com>
2018-02-09 03:33:50 -0800

    finished park manager UI

--- src/UI/UrbanParkUI.java
+++ src/UI/UrbanParkUI.java
@@ -2,6 +2,7 @@ package UI;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
@@ -21,15 +22,15 @@ public class UrbanParkUI {
 
 	private static final String USERS_DATA_FILE = "UsersInformations.ser";
 
-	private static final String myLogOut = "        3. Log out";
-
 	private static final String WHAT_WOULD_LIKE_TO_DO_MESSAGE = ">>> What would you like to do?";
 
 	private static final String SELECT_A_NUMBER_MESSAGE = "    (Please select a number)";
 
 	private static final String USER_INPUT_MESSAGE = "> ";
 
-	private static final String YES_OR_NO_MESSAGE = "    (Please enter Yes or No)";
+	private static final String YES_OR_NO_MESSAGE = "    (Please enter Yes or No)\n";
+
+	private static final String INVALID_INPUT_MESSAGE = ">>> Please enter a valid option.\n";
 
 	private static Scanner myScanner;
 
@@ -47,7 +48,7 @@ public class UrbanParkUI {
 
 	public static void main(final String[] theArgs) {
 		init();
-		while(welcome()) {
+		while (welcome()) {
 			if (myCurrentUser instanceof ParkManager) {
 				myParkManager = (ParkManager) myCurrentUser;
 				basicParkManagerOptions();
@@ -61,10 +62,10 @@ public class UrbanParkUI {
 	private static void init() {
 		myJobs = new JobMap();
 		myUsers = new UserMap();
-				
+
 		myJobs.loadJobMap(JOBS_DATA_FILE);
 		myUsers.loadUserMap(USERS_DATA_FILE);
-		
+
 		myScanner = new Scanner(System.in);
 		myDateFormatter = DateTimeFormatter.ofPattern("MM/dd/uu");
 	}
@@ -84,12 +85,12 @@ public class UrbanParkUI {
 			System.out.println("        1. Log in");
 			System.out.println("        2. Exit\n");
 			int selection = 0;
-			while(!isExit) {
+			while (!isExit) {
 				System.out.print(USER_INPUT_MESSAGE);
 				try {
 					selection = myScanner.nextInt();
 					myScanner.nextLine();
-				} catch (InputMismatchException e) {
+				} catch (final InputMismatchException theException) {
 					myScanner = new Scanner(System.in);
 				}
 				System.out.println();
@@ -105,30 +106,30 @@ public class UrbanParkUI {
 					isExitProgram = false;
 					break;
 				default:
-					System.out.println(">>> Please enter a valid option.\n");
+					System.out.println(INVALID_INPUT_MESSAGE);
 					break;
 				}
 			}
-			
+
 		}
 		return isExitProgram;
 	}
 
 	private static void logIn() {
 		System.out.println(">>> Please enter your username:\n");
-		while(true) {
+		while (true) {
 			System.out.print(USER_INPUT_MESSAGE);
 			String username = myScanner.nextLine();
 			System.out.println();
 			myCurrentUser = myUsers.getUser(username);
-			if(myCurrentUser == null) {
+			if (myCurrentUser == null) {
 				System.out.println(">>> Your username does not exist.");
 				System.out.println(">>> Please enter a different username:\n");
 			} else {
 				break;
 			}
 		}
-		
+
 	}
 
 	private static void welcomeUserMessage() {
@@ -142,94 +143,159 @@ public class UrbanParkUI {
 			welcomeUserMessage();
 			System.out.println(WHAT_WOULD_LIKE_TO_DO_MESSAGE);
 			System.out.println(SELECT_A_NUMBER_MESSAGE);
-			System.out.println("        1. Submit a new job");
-			System.out.println("        2. View all your submitted jobs");
-			System.out.println(myLogOut + '\n');
-			System.out.print(USER_INPUT_MESSAGE);
-			int selection = myScanner.nextInt();
-			myScanner.nextLine();
-			System.out.println();
-			switch (selection) {
-			case 1:
-				submitNewJob();
-				break;
-			case 2:
-				printParkManagerSubmittedJobs(true);
-				break;
-			default:
-				isExit = true;
-				break;
+			String[] message = { ". Submit a new job", ". View all your submitted jobs", ". Log out" };
+			int index = 0;
+			int choiceModifier = 0;
+			if (!myParkManager.isLessThanMaxJobs(myJobs)) {
+				index = 1;
+				choiceModifier = 1;
+			}
+			for (int i = 1; index < message.length; i++, index++) {
+				System.out.println("        " + i + message[index]);
+			}
+			while (true) {
+				System.out.print(USER_INPUT_MESSAGE);
+				int selection = 0;
+				try {
+					selection = myScanner.nextInt();
+					myScanner.nextLine();
+				} catch (final InputMismatchException theException) {
+					myScanner = new Scanner(System.in);
+				}
+				System.out.println();
+				if (myParkManager.isLessThanMaxJobs(myJobs) && selection == 1) {
+					submitNewJob();
+					break;
+				} else if (selection + choiceModifier == 2) {
+					printParkManagerSubmittedJobs(true);
+					break;
+				} else if (selection + choiceModifier == 3) {
+					isExit = true;
+					break;
+				} else {
+					System.out.println(INVALID_INPUT_MESSAGE);
+				}
 			}
 		}
 	}
 
 	private static void submitNewJob() {
-		System.out.println(">>> Please fill out the job details.");
-		System.out.println(">>> Enter park name:\n");
-		System.out.print(USER_INPUT_MESSAGE);
-		String parkName = myScanner.nextLine();
-		System.out.println();
+		boolean isFillOutJob = true;
+		while (isFillOutJob) {
+			System.out.println(">>> Please fill out the job details.");
+			System.out.println(">>> Enter park name:\n");
+			System.out.print(USER_INPUT_MESSAGE);
+			String parkName = myScanner.nextLine();
+			System.out.println();
 
-		System.out.println(">>> Enter job location:\n");
-		System.out.print(USER_INPUT_MESSAGE);
-		String jobLocation = myScanner.nextLine();
-		System.out.println();
+			System.out.println(">>> Enter job location:\n");
+			System.out.print(USER_INPUT_MESSAGE);
+			String jobLocation = myScanner.nextLine();
+			System.out.println();
 
-		System.out.println(">>> Enter job start date(MM/DD/YY):\n");
-		System.out.print(USER_INPUT_MESSAGE);
-		LocalDate jobStartDate = LocalDate.parse(myScanner.nextLine(), myDateFormatter);
-		System.out.println();
+			System.out.println(">>> Enter job start date(MM/DD/YY):\n");
 
-		System.out.println(">>> Enter job end date (MM/DD/YY):\n");
-		System.out.print(USER_INPUT_MESSAGE);
-		LocalDate jobEndDate = LocalDate.parse(myScanner.nextLine(), myDateFormatter);
-		System.out.println();
+			LocalDate jobStartDate = null;
+			boolean isExit = false;
+			while (!isExit) {
+				try {
+					System.out.print(USER_INPUT_MESSAGE);
+					jobStartDate = LocalDate.parse(myScanner.nextLine(), myDateFormatter);
+					isExit = true;
+				} catch (final DateTimeParseException theException) {
+					System.out.println("\n>>> Invalid date. Please use MM/DD/YY format.\n");
+				}
+			}
 
-		System.out.println(">>> Enter job description:\n");
-		System.out.print(USER_INPUT_MESSAGE);
-		String jobDescription = myScanner.nextLine();
-		System.out.println();
+			System.out.println();
 
-		Job job = new Job(jobStartDate, jobEndDate, parkName, myParkManager, jobLocation, jobDescription);
-		System.out.println(job);
-		System.out.println();
+			System.out.println(">>> Enter job end date (MM/DD/YY):\n");
 
-		if (myParkManager.isJobEndsWithinMaxDays(job) && myParkManager.isJobWithinMaxDays(job)) {
-			jobDetailsVerification(job);
-			printParkManagerSubmittedJobs(false);
+			LocalDate jobEndDate = null;
+
+			isExit = false;
+			while (!isExit) {
+				try {
+					System.out.print(USER_INPUT_MESSAGE);
+					jobEndDate = LocalDate.parse(myScanner.nextLine(), myDateFormatter);
+					isExit = true;
+				} catch (final DateTimeParseException theException) {
+					System.out.println("\n>>> Invalid date. Please use MM/DD/YY format.\n");
+				}
+			}
+			System.out.println();
+
+			System.out.println(">>> Enter job description:\n");
+			System.out.print(USER_INPUT_MESSAGE);
+			String jobDescription = myScanner.nextLine();
+			System.out.println();
+
+			Job job = new Job(jobStartDate, jobEndDate, parkName, myParkManager, jobLocation, jobDescription);
+			System.out.println(job);
+			System.out.println();
+
+			if (!myParkManager.isJobEndsWithinMaxDays(job) || !myParkManager.isJobWithinMaxDays(job)) {
+				System.out.println("We cannot submit this job:");
+				if (!myParkManager.isJobWithinMaxDays(job)) {
+					System.out.println("Your job takes more than " + myParkManager.getMaxJobLength() + " days.");
+				}
+				if (!myParkManager.isJobEndsWithinMaxDays(job)) {
+					System.out.println("Your job ends more than " + myParkManager.getMaxEndDay() + " days from today.");
+				}
+				System.out.println();
+			} else {
+				jobDetailsVerification(job);
+				printParkManagerSubmittedJobs(false);
+			}
+
+			System.out.println(">>> Would you like to submit another job?");
+			System.out.println(YES_OR_NO_MESSAGE);
+
+			isExit = false;
+			while (!isExit) {
+				System.out.print(USER_INPUT_MESSAGE);
+				switch (myScanner.nextLine().toLowerCase()) {
+				case "yes":
+					System.out.println();
+					isExit = true;
+					break;
+				case "no":
+					System.out.println();
+					isExit = true;
+					isFillOutJob = false;
+					break;
+				default:
+					System.out.println(INVALID_INPUT_MESSAGE);
+					break;
+				}
+			}
 		}
 
-		System.out.println(">>> Would you like to submit another job?");
-		System.out.println(YES_OR_NO_MESSAGE + '\n');
-		System.out.print(USER_INPUT_MESSAGE);
-		switch (myScanner.nextLine().toLowerCase()) {
-		case "yes":
-			submitNewJob();
-			break;
-		case "no":
-			break;
-		default:
-			System.out.println("Invalid input");
-			break;
-		}
 	}
 
 	private static void jobDetailsVerification(final Job theJob) {
+		boolean isExit = false;
 		System.out.println(">>> Would like to submit this job?");
-		System.out.println(YES_OR_NO_MESSAGE + '\n');
-		System.out.print(USER_INPUT_MESSAGE);
-		switch (myScanner.nextLine().toLowerCase()) {
-		case "yes":
-
-			myJobs.addJob(theJob);
-			myParkManager.createJob(theJob);
-
-			myJobs.storeJobMap(JOBS_DATA_FILE);
-			myUsers.storeUserMap(USERS_DATA_FILE);
-			System.out.println("\n>>> Job has been submitted successfully.");
-			break;
-		default:
-			break;
+		System.out.println(YES_OR_NO_MESSAGE);
+		while (!isExit) {
+			System.out.print(USER_INPUT_MESSAGE);
+			switch (myScanner.nextLine().toLowerCase()) {
+			case "yes":
+				isExit = true;
+				myJobs.addJob(theJob);
+				myParkManager.createJob(theJob);
+				myJobs.storeJobMap(JOBS_DATA_FILE);
+				myUsers.storeUserMap(USERS_DATA_FILE);
+				System.out.println("\n>>> Job has been submitted successfully.");
+				break;
+			case "no":
+				System.out.println("\n>>> Job has been cancelled successfully.");
+				isExit = true;
+				break;
+			default:
+				System.out.println(INVALID_INPUT_MESSAGE);
+				break;
+			}
 		}
 	}
 
@@ -242,32 +308,60 @@ public class UrbanParkUI {
 				System.out.print("    " + i + ". ");
 				System.out.print(job.getParkName());
 				System.out.print(": " + job.getStartDate().format(myDateFormatter));
-				System.out.println(" - " + job.getEndDate().format(myDateFormatter) + '\n');
+				System.out.println(" - " + job.getEndDate().format(myDateFormatter));
 			}
-
+			if (jobIDList.size() == 0) {
+				System.out.println("    You have not submitted any jobs.");
+			}
+			System.out.println();
 			if (isAbleToViewDetails) {
 				System.out.println("    0. Return to previous menu");
 				System.out.println("    (Please select a number to view job details)\n");
-				System.out.print(USER_INPUT_MESSAGE);
-				int selection = myScanner.nextInt();
-				myScanner.nextLine();
-				if (selection > 0 && selection <= jobIDList.size()) {
-					System.out.println(myJobs.getJob(jobIDList.get(selection - 1)));
-					System.out.println(">>> Do you want to view another job details?");
-					System.out.println(YES_OR_NO_MESSAGE);
+				while (true) {
 					System.out.print(USER_INPUT_MESSAGE);
-					switch (myScanner.nextLine().toLowerCase()) {
-					case "no":
-						isAbleToViewDetails = false;
+					int selection = -1;
+					try {
+						selection = myScanner.nextInt();
+						myScanner.nextLine();
+					} catch (final InputMismatchException theException) {
+						myScanner = new Scanner(System.in);
 					}
-				} else if (selection == 0) {
-					isAbleToViewDetails = false;
-				} else {
-					System.out.println("Invalid input");
+
+					System.out.println();
+					if (selection > 0 && selection <= jobIDList.size()) {
+						System.out.println(myJobs.getJob(jobIDList.get(selection - 1)));
+						System.out.println(">>> Do you want to view another job details?");
+						System.out.println(YES_OR_NO_MESSAGE);
+						boolean isExit = false;
+						while (!isExit) {
+							System.out.print(USER_INPUT_MESSAGE);
+							switch (myScanner.nextLine().toLowerCase()) {
+							case "yes":
+								isExit = true;
+								System.out.println();
+								break;
+							case "no":
+								isAbleToViewDetails = false;
+								isExit = true;
+								System.out.println();
+								break;
+							default:
+								System.out.println();
+								System.out.println(INVALID_INPUT_MESSAGE);
+								break;
+							}
+						}
+						break;
+					} else if (selection == 0) {
+						isAbleToViewDetails = false;
+						break;
+					} else {
+						System.out.println(INVALID_INPUT_MESSAGE);
+					}
 				}
+
 			}
 		} while (isAbleToViewDetails);
-
 	}
 
 	private static void basicVolunteerOptions() {
@@ -276,7 +370,7 @@ public class UrbanParkUI {
 		System.out.println(SELECT_A_NUMBER_MESSAGE);
 		System.out.println("        1. Sign up for a new job");
 		System.out.println("        2. View your upcoming jobs");
-		System.out.println(myLogOut);
+		System.out.println("        3. Log out");
 		System.out.println();
 		System.out.print(USER_INPUT_MESSAGE);
 		int selection = myScanner.nextInt();
