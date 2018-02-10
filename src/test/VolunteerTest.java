8692e3de406fd1f5014de178a850743092eaddad
Matthew Chen <matthewfory@gmail.com>
2018-02-09 16:28:50 -0800

    change line length

--- src/test/VolunteerTest.java
+++ src/test/VolunteerTest.java
@@ -1,9 +1,7 @@
-/**
- * T CSS 360 - Winter 2018
- * Team: Group 7
+/*
+ * TCSS 360 - Winter 2018
  * Urban Parks Project
  */
-
 package test;
 
 import static org.junit.Assert.*;
@@ -18,35 +16,37 @@ import model.ParkManager;
 import model.Volunteer;
 
 /**
- * Test for business rule 1a.
- * 1) As a Volunteer I want to sign up for a job
- * 		b) A volunteer may sign up only if the job begins at least a minimum number of calendar days after the current date, default of 2.
-		   i)   Volunteer signs up for job that begins much more than the minimum number of calendar days from the current date.
-		   ii)  Volunteer signs up for job that begins exactly the minimum number of calendar days from the current date.
-		   iii) Volunteer signs up for job that begins less than the minimum number of calendar days from the current date.
- */
-
-/**
- * @author Minqing Chen
- * @version 02/04/2018
+ * JUnit tests for volunteer.
+ * @author Group 7
+ * @version February 9, 2018
  *
  */
 public class VolunteerTest {
 	
-	private static final LocalDate myFirstJobStartDate = LocalDate.now().plusDays(10);
-	private static final LocalDate myFirstJobEndDate = myFirstJobStartDate;
+	private static final LocalDate myFirstJobStartDate = 
+			LocalDate.now().plusDays(10);
+	private static final LocalDate myFirstJobEndDate = 
+			myFirstJobStartDate;
 	
-	private static final LocalDate mySecondJobStartDate = LocalDate.now().plusDays(16);
-	private static final LocalDate mySecondJobEndDate = mySecondJobStartDate.plusDays(2);
+	private static final LocalDate mySecondJobStartDate = 
+			LocalDate.now().plusDays(16);
+	private static final LocalDate mySecondJobEndDate = 
+			mySecondJobStartDate.plusDays(2);
 	
-	private static final LocalDate myFirstJobCandidateStartDate = LocalDate.now().plusDays(13);
-	private static final LocalDate myFirstJobCandidateEndDate = myFirstJobCandidateStartDate.plusDays(2);
+	private static final LocalDate myFirstJobCandidateStartDate = 
+			LocalDate.now().plusDays(13);
+	private static final LocalDate myFirstJobCandidateEndDate = 
+			myFirstJobCandidateStartDate.plusDays(2);
 	
-	private static final LocalDate mySecondJobCandidateStartDate = LocalDate.now().plusDays(18);
-	private static final LocalDate mySecondJobCandidateEndDate = mySecondJobCandidateStartDate;
+	private static final LocalDate mySecondJobCandidateStartDate = 
+			LocalDate.now().plusDays(18);
+	private static final LocalDate mySecondJobCandidateEndDate = 
+			mySecondJobCandidateStartDate;
 	
-	private static final LocalDate myThirdJobCandidateStartDate = LocalDate.now().plusDays(16);
-	private static final LocalDate myThirdJobCandidateEndDate = myThirdJobCandidateStartDate;
+	private static final LocalDate myThirdJobCandidateStartDate = 
+			LocalDate.now().plusDays(16);
+	private static final LocalDate myThirdJobCandidateEndDate = 
+			myThirdJobCandidateStartDate;
 	
 	private Job myJobLessThanTwoDaysAway;
 	
@@ -54,13 +54,17 @@ public class VolunteerTest {
 
 	private Job myJobMoreThanTwoDayAway;			
 	
-	private static final LocalDate myJobStartDateLessThanTwoDaysAway = LocalDate.now().plusDays(1);
+	private static final LocalDate myJobStartDateLessThanTwoDaysAway = 
+			LocalDate.now().plusDays(1);
 	
-	private static final LocalDate myJobStartDateTwoDaysAway = LocalDate.now().plusDays(2);
+	private static final LocalDate myJobStartDateTwoDaysAway = 
+			LocalDate.now().plusDays(2);
 	
-	private static final LocalDate myJobStartDateMoreThanTwoDaysAway = LocalDate.now().plusDays(20);
+	private static final LocalDate myJobStartDateMoreThanTwoDaysAway = 
+			LocalDate.now().plusDays(20);
 	
-	private static final LocalDate myJobEndDate = LocalDate.now().plusDays(0); 
+	private static final LocalDate myJobEndDate = 
+			LocalDate.now().plusDays(0); 
 	
 	private ParkManager myParkManager;
 	
@@ -87,19 +91,33 @@ public class VolunteerTest {
 		myParkManager = new ParkManager("brook", "Brook", "Negussie");
 		myJobMap = new JobMap();
 		
-		myFirstJob = new Job(myFirstJobStartDate, myFirstJobEndDate, "Discovery Park", myParkManager, "Seattle", "Pick up leaves");
-		mySecondJob = new Job(mySecondJobStartDate, mySecondJobEndDate, "Seward Park", myParkManager, "Seattle", "Pick up leaves");
+		myFirstJob = new Job(myFirstJobStartDate, myFirstJobEndDate, 
+				"Discovery Park", myParkManager, "Seattle", "Pick up leaves");
+		mySecondJob = new Job(mySecondJobStartDate, mySecondJobEndDate, 
+				"Seward Park", myParkManager, "Seattle", "Pick up leaves");
 		
-		myFirstJobCandidate = new Job(myFirstJobCandidateStartDate, myFirstJobCandidateEndDate, "Volunteer Park", myParkManager, "Seattle", "Pick up leaves");
-		mySecondJobCandidate = new Job(mySecondJobCandidateStartDate, mySecondJobCandidateEndDate, "Gas Works Park", myParkManager, "Seattle", "Pick up leaves");
-		myThirdJobCandidate = new Job(myThirdJobCandidateStartDate, myThirdJobCandidateEndDate, "Lincoln Park", myParkManager, "Seattle", "Pick up leaves");
+		myFirstJobCandidate = new Job(myFirstJobCandidateStartDate, 
+				myFirstJobCandidateEndDate, "Volunteer Park", 
+				myParkManager, "Seattle", "Pick up leaves");
+		mySecondJobCandidate = new Job(mySecondJobCandidateStartDate, 
+				mySecondJobCandidateEndDate, "Gas Works Park", myParkManager, 
+				"Seattle", "Pick up leaves");
+		myThirdJobCandidate = new Job(myThirdJobCandidateStartDate, 
+				myThirdJobCandidateEndDate, "Lincoln Park", myParkManager, 
+				"Seattle", "Pick up leaves");
 		myJobMap.addJob(myFirstJob);
 		myJobMap.addJob(mySecondJob);
 		
 		myAnyVolunteer = new Volunteer("Hasnah Said", "Hasnah", "Said");
-		myJobLessThanTwoDaysAway = new Job(myJobStartDateLessThanTwoDaysAway, myJobEndDate, "Discover Park", myParkManager, "Seattle, WA", "Pick up leaves");
-		myJobEqualsToTwoDaysAway = new Job(myJobStartDateTwoDaysAway, myJobEndDate, "Cherry Park", myParkManager, "Seattle, WA", "Pick up leaves");
-		myJobMoreThanTwoDayAway = new Job(myJobStartDateMoreThanTwoDaysAway, myJobEndDate, "Kerry Park", myParkManager, "Seattle, WA", "Pick up leaves");
+		myJobLessThanTwoDaysAway = new Job(myJobStartDateLessThanTwoDaysAway, 
+				myJobEndDate, "Discover Park", myParkManager, "Seattle, WA", 
+				"Pick up leaves");
+		myJobEqualsToTwoDaysAway = new Job(myJobStartDateTwoDaysAway, 
+				myJobEndDate, "Cherry Park", myParkManager, "Seattle, "
+						+ "WA", "Pick up leaves");
+		myJobMoreThanTwoDayAway = new Job(myJobStartDateMoreThanTwoDaysAway, 
+				myJobEndDate, "Kerry Park", myParkManager, "Seattle, WA", 
+				"Pick up leaves");
 	}
 	
 	/**
@@ -109,7 +127,8 @@ public class VolunteerTest {
 	@Test
 	public void isSameDayConflict_VolunteerHasNoCurrentJobs_False() {
 		for (int jobID : myVolunteer.getJobList()) {
-			assertFalse(myVolunteer.isSameDayConflict(myFirstJobCandidate, myJobMap.getJob(jobID)));
+			assertFalse(myVolunteer.isSameDayConflict(myFirstJobCandidate, 
+					myJobMap.getJob(jobID)));
 		}
 	}
 	
@@ -123,7 +142,8 @@ public class VolunteerTest {
 		myVolunteer.signup(mySecondJob);
 		
 		for (int jobID : myVolunteer.getJobList()) {
-			assertFalse(myVolunteer.isSameDayConflict(myFirstJobCandidate, myJobMap.getJob(jobID)));
+			assertFalse(myVolunteer.isSameDayConflict(myFirstJobCandidate, 
+					myJobMap.getJob(jobID)));
 		}
 	}
 	
@@ -137,7 +157,8 @@ public class VolunteerTest {
 		myVolunteer.signup(mySecondJob);
 		
 		for (int jobID : myVolunteer.getJobList()) {
-			assertFalse(myVolunteer.isSameDayConflict(mySecondJobCandidate, myJobMap.getJob(jobID)));
+			assertFalse(myVolunteer.isSameDayConflict(mySecondJobCandidate, 
+					myJobMap.getJob(jobID)));
 		}
 	}
 	
@@ -151,7 +172,8 @@ public class VolunteerTest {
 		myVolunteer.signup(mySecondJob);
 		
 		for (int jobID : myVolunteer.getJobList()) {
-			assertFalse(myVolunteer.isSameDayConflict(myThirdJobCandidate, myJobMap.getJob(jobID)));
+			assertFalse(myVolunteer.isSameDayConflict(myThirdJobCandidate, 
+					myJobMap.getJob(jobID)));
 		}
 		myJobLessThanTwoDaysAway = new Job(myJobStartDateLessThanTwoDaysAway, myJobEndDate, 
 				"Discover Park", myParkManager, "Seattle, WA", "clean");
