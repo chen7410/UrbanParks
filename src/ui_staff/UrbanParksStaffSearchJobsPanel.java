de1a6d79ad00bdaea7d353c1dc0b37b3585b4d0e
brookn3 <brooknegussie2014@gmail.com>
2018-03-01 02:01:53 -0800

    working on converting string input into a Date object in the StaffSearchJobsPanel class and created a test class to see how it looks.

--- src/ui_staff/UrbanParksStaffSearchJobsPanel.java
+++ src/ui_staff/UrbanParksStaffSearchJobsPanel.java
@@ -6,21 +6,29 @@
 package ui_staff;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
 import java.awt.font.TextAttribute;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
+import java.util.Observable;
 
+import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
+import ui.ButtonSignal;
 import ui.GUI;
 
 /**
@@ -29,7 +37,7 @@ import ui.GUI;
  * @author Brook Negussie
  * @version March 5, 2018
  */
-public class UrbanParksStaffSearchJobsPanel {
+public class UrbanParksStaffSearchJobsPanel extends Observable {
 	
 	private JPanel myPanel;
 	
@@ -58,19 +66,83 @@ public class UrbanParksStaffSearchJobsPanel {
 		
 		searchJobsLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 		
-		JPanel datesPanel = new JPanel(new FlowLayout());
+		JPanel startDatePanel = new JPanel(new FlowLayout());
+		JPanel endDatePanel = new JPanel(new FlowLayout());
 		
-		JLabel startDateLabel = new JLabel("Start date: ", SwingConstants.CENTER);
+		JLabel startDateLabel = new JLabel("Start date (MM/dd/yyyy): ", SwingConstants.CENTER);
+		JTextField startDateInputField = new JTextField("", 11);
 		
-		// Adding a calendar to receive input for start date.
+		JLabel endDateLabel = new JLabel("Start date (MM/dd/yyyy): ", SwingConstants.CENTER);
+		JTextField endDateInputField = new JTextField("", 11);
 		
 		
+		// Panel for buttons at the south quadrant of the frame.
+		JPanel buttonsPanel = new JPanel(new FlowLayout());
 		
-		datesPanel.add(startDateLabel);
+		JButton homeButton = new JButton(
+				new AbstractAction("Home") {
+
+			/** */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				setChanged();
+				notifyObservers(new ButtonSignal("home", 0));
+			}
+		});
+		homeButton.setSize(GUI.BUTTON_SIZE);
+		homeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		
+		try {
+			SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+			Date startDate = dateFormat.parse(startDateInputField.getText());
+			Date endDate = dateFormat.parse(endDateInputField.getText());
+			
+			JButton submitButton = createRadioButton(startDate, endDate);
+			
+			buttonsPanel.add(homeButton);
+			buttonsPanel.add(submitButton);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		startDatePanel.add(startDateLabel);
+		startDatePanel.add(startDateInputField);
+		
+		endDatePanel.add(endDateLabel);
+		endDatePanel.add(endDateInputField);
 		
 		
 		mainPanel.add(Box.createRigidArea(new Dimension(0, 75)));
 		mainPanel.add(searchJobsLabel);
 		mainPanel.add(Box.createRigidArea(new Dimension(0, 50)));
+		mainPanel.add(startDatePanel);
+		mainPanel.add(Box.createRigidArea(new Dimension(0, 25)));
+		mainPanel.add(startDatePanel);
+		
+		myPanel.add(mainPanel, BorderLayout.CENTER);
+		myPanel.add(buttonsPanel, BorderLayout.SOUTH);
 	}
+	
+	public JButton createRadioButton(final Date theStartDate, final Date theEndDate) {
+		JButton button = new JButton(
+				new AbstractAction("Submit") {
+
+			/** */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO: something 
+			}
+		});
+		return button;
+	}
+	
+	public JPanel getPanel() {
+		return myPanel;
+	}	
 }
