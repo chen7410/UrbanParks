879dceb8db5e94b9f26757db646058719a291d4a
HasnahSaid <hasna024@uw.edu>
2018-03-01 14:15:15 -0800

    park manager confirmation panels added

---
+++ src/test_ui/ParkManagerSubmitConfirmationTest.java
@@ -0,0 +1,49 @@
+package test_ui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+import model.JobMap;
+import model.ParkManager;
+import model.UserMap;
+import ui_park_manager.ParkManagerSubmitConfirmationPanel;
+
+/**
+ * Test for ParkMangaer Confirmation
+ * @author Hasnah Said
+ * @version March 1, 2018
+ */
+
+public class ParkManagerSubmitConfirmationTest {
+
+	/** The main window. */
+    private static JFrame myFrame;
+    
+    private static UserMap myUsers;
+    private static JobMap myJobs;
+    private static ParkManager myParkManager;
+    
+    private static ParkManagerSubmitConfirmationPanel myConfirmationPanel;
+    
+    public static void main (final String[] theArgs) {
+    	myFrame = new JFrame("UrbanParks");
+    	
+    	myJobs = new JobMap();
+		myUsers = new UserMap();
+		myUsers.loadUserMap(UserMap.USERS_DATA_FILE);
+		myJobs.loadJobMap(JobMap.JOBS_DATA_FILE);
+		
+		myParkManager = (ParkManager) myUsers.getUser("brook");
+		myConfirmationPanel = new ParkManagerSubmitConfirmationPanel(
+				myParkManager.getJobList(myJobs).get(0), myParkManager.getJobList(myJobs));
+    	
+		myFrame.setTitle("Urban Parks - Park Manager - " + myParkManager.getLastName());
+		myFrame.add(myConfirmationPanel.getPanel(), BorderLayout.CENTER);
+		myFrame.pack();
+    	myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	myFrame.setLocationRelativeTo(null);
+        myFrame.setVisible(true);
+    }
+    
+}
---
+++ src/ui_staff/UrbanParksStaffChangeMaxJobAmountPanel.java
@@ -0,0 +1,105 @@
+package ui_staff;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Observable;
+
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import model.JobMap;
+import ui.ButtonSignal;
+import ui.GUI;
+
+public class UrbanParksStaffChangeMaxJobAmountPanel extends Observable {
+	
+	private JPanel myPanel;
+	private JobMap myJobs;
+	private int myCurrentNumOfPendingJobs;
+	
+	public UrbanParksStaffChangeMaxJobAmountPanel(final JobMap theJobs) {
+		myPanel = new JPanel(new BorderLayout());
+		myJobs = theJobs;
+		myCurrentNumOfPendingJobs = myJobs.getMaxJobAmount();
+		setUp();
+	}
+	
+	private void setUp() {
+		myPanel.setPreferredSize(GUI.PANEL_SIZE);
+		createButtons();
+		createChangeNumberPanel();
+	}
+	
+
+	private void createButtons() {
+		JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER,
+				GUI.BUTTON_GAP_WIDTH,
+				GUI.BUTTON_GAP_HEIGHT));
+		JButton submitChangeButton = new JButton(new AbstractAction("Submit Changes") {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//TODO: Observer?
+				setChanged();
+				notifyObservers(new ButtonSignal("submit change", 0));
+			}
+		});
+		
+		submitChangeButton.setPreferredSize(GUI.BUTTON_SIZE);
+		buttonsPanel.add(submitChangeButton);
+		buttonsPanel.setBackground(GUI.VOLUNTEER_PANELS_BGCOLOR);
+		myPanel.add(buttonsPanel, BorderLayout.SOUTH);
+	}
+	
+	
+	private void createChangeNumberPanel() {
+		JPanel changeNumberPanel = new JPanel(new GridLayout(0, 1));
+		changeNumberPanel.setBackground(Color.WHITE);
+		JLabel currentMaxLabel = new JLabel("Current number of maximum pending jobs: "
+				+ myCurrentNumOfPendingJobs);
+		currentMaxLabel.setFont(new Font(null, Font.BOLD, 20));
+		changeNumberPanel.add(currentMaxLabel);
+		
+		JLabel headerLabel = new JLabel("Please enter the new number in the box below.");
+		headerLabel.setFont(new Font(null, Font.BOLD, 20));
+		changeNumberPanel.add(headerLabel);
+		
+		JPanel textPanel = new JPanel();
+		textPanel.setBackground(Color.WHITE);
+		JLabel label = new JLabel("New Number: ");
+		textPanel.add(label);
+		
+		//TODO: ActionListener?
+		JTextField textField = new JTextField(20);
+		textField.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String str = textField.getText();
+				int newNumber = Integer.parseInt(str);
+				myJobs.setMaxJobAmount(newNumber);	
+			}
+		});
+		
+		textPanel.add(textField);
+
+		
+		changeNumberPanel.add(textPanel, BorderLayout.WEST);
+		
+		myPanel.add(changeNumberPanel, BorderLayout.CENTER);
+	}
+
+	public JPanel getPanel() {
+		return myPanel;
+	}
+}
